b6bebbc7b5acd7f22ac432859c1a1a337d9c6c28
DawidSkrobisz <dawidskrobisz88@gmail.com>
2023-09-15 19:43:05 +0200

    firstService

---
+++ src/main/java/dawidskrobisz/com/BookShop/Book.java
@@ -0,0 +1,27 @@
+package dawidskrobisz.com.BookShop;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+    Long id;
+    String title;
+    String author;
+    Integer year;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
--- src/main/java/dawidskrobisz/com/BookShop/BookShopApplication.java
+++ src/main/java/dawidskrobisz/com/BookShop/BookShopApplication.java
@@ -1,13 +1,25 @@
 package dawidskrobisz.com.BookShop;
 
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+@AllArgsConstructor
 @SpringBootApplication
-public class BookShopApplication {
+public class BookShopApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookShopApplication.class, args);
 	}
 
+	private final CatalogService catalogService;
+
+	@Override
+	public void run(String... args) {
+		List<Book> books = catalogService.findByTitle("Hobbit");
+		books.forEach(System.out::println);
+
+	}
 }
