a16fd037249a684c26cf703bccc40a1eb899b23b
DawidSkrobisz <dawidskrobisz88@gmail.com>
2023-09-16 12:41:46 +0200

    PrimaryBean

--- src/main/java/dawidskrobisz/com/BookShop/catalog/domain/CatalogService.java
+++ src/main/java/dawidskrobisz/com/BookShop/catalog/domain/CatalogService.java
@@ -1,16 +1,18 @@
 package dawidskrobisz.com.BookShop.catalog.domain;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 @Service
-@RequiredArgsConstructor
 public class CatalogService {
-
-
     private final CatalogRepository repository;
+
+    public CatalogService(@Qualifier("bestsellerCatalogRepository") CatalogRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Book> findByTitle(String title) {
         return repository.listAll()
--- src/main/java/dawidskrobisz/com/BookShop/catalog/infrastructure/SchoolCatalogRepository.java
+++ src/main/java/dawidskrobisz/com/BookShop/catalog/infrastructure/SchoolCatalogRepository.java
@@ -9,14 +9,14 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 @Repository
-public class MemoryCatalogRepository implements CatalogRepository {
+class SchoolCatalogRepository implements CatalogRepository {
     private final Map<Long, Book> storage = new ConcurrentHashMap<>();
 
-    public MemoryCatalogRepository() {
-        storage.put(1L, new Book(1L, "Wladca Pierscieni", "Tolkien", 1975));
-        storage.put(2L, new Book(1L, "Hobbit", "Tolkien", 1970));
-        storage.put(3L, new Book(1L, "Pieśń Lodu i Ognia", "Martin", 1975));
-        storage.put(4L, new Book(1L, "Hobbit 2", "Tolkien", 1970));
+    public SchoolCatalogRepository() {
+        storage.put(1L, new Book(1L, "Ogniem i Mieczem", "Sienkiewicz", 1975));
+        storage.put(2L, new Book(1L, "Krzyżacy", "Sienkiewicz", 1970));
+        storage.put(3L, new Book(1L, "Pan Tadeusz", "Mickiewicz", 1975));
+        storage.put(4L, new Book(1L, "Nad Niemnem", "Orzeszkowa", 1970));
     }
 
     @Override
